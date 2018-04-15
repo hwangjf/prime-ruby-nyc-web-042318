f0c7f4f903c3b6f0115f80ea52560c520eaabc27
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-15 23:20:33 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,11 @@
 def prime? (int)
-  return false if int < 0
-  for i in 2.. ((int)-1)
-    if int % i == 0 
-      return false
+  if int > 0
+    for i in 2.. ((int)-1)
+      if int % i == 0 
+        return false
+      end
     end
-  end
-  true
+    true
+  else
+    return false
 end
