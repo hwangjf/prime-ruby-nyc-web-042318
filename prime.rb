250aae625c28177f23c9b73b0ba37f20242930fd
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-15 23:11:35 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+
+def prime? (integer)
+  i = 2
+  until i == integer.abs
+    x = integer.abs % i
+    i += 1
+    if x == 0
+      return false
+      break
+    end
+  end
+end
